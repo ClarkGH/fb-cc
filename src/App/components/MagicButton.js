ec78f81bc4ec507ada2e0ae86de69dc527306e9f
clarkgh <demolisho@hotmail.com>
2018-08-28 06:19:00 -0700

    changed to functional component

--- src/App/components/MagicButton.js
+++ src/App/components/MagicButton.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (<button onClick={this.props.handleClick}>{this.props.children}</button>);
-  }
+function MagicButton (props) {
+    return (<button onClick={props.handleClick}>{props.children}</button>);
 }
 
-export default App;
+export default MagicButton;
